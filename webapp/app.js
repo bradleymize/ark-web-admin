445ffdc3c554b8de90ece09bf83714111da12278
Bradley Mize <general_bradley@hotmail.com>
2015-09-17 21:54:41 -0700

    Updated config for https support

--- webapp/app.js
+++ webapp/app.js
@@ -5,14 +5,17 @@ var path = require('path');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var favicon = require('serve-favicon');
+var Datastore = require('nedb');
 
 var app = express();
 app.use(express.static(path.join(__dirname, 'public')));
 
-var port = 9000;
+var protocol = process.argv[2] || "http"
+var port = protocol === "https" ? 443 : 80;
 
 // uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -22,15 +25,20 @@ app.get('*', function(req, res) {
   res.sendFile('./public/index.html');
 });
 
-//app.listen(port); //http
+//=========================================
+//TODO: npm install bcrypt --save
+//TODO: install Visual C++ VCBuild.exe (.NET Framework 2.0)
+var ds = new Datastore({filename: './users.db', autoload: true});
+//=========================================
 
-// https://letsencrypt.org/howitworks/
-var options = {
-  key: fs.readFileSync('key.pem'),
-  cert: fs.readFileSync('cert.pem')
+if(protocol === "https") {
+  https.createServer({
+    key: fs.readFileSync('../key.pem'),
+    cert: fs.readFileSync('../cert.pem')
+  },app).listen(port);
+} else {
+  app.listen(port);
 }
-//TODO: if env = dev, self-sign certificate
-https.createServer(options,app).listen(443);
-console.log("Server running: https://localhost:"+port);
+console.log("Server running: "+protocol+"://localhost");
 
 module.exports = app;
