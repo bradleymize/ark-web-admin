a51a207560a444a4190f01976de31eabb3853bfa
Bradley Mize <general_bradley@hotmail.com>
2015-09-19 22:25:02 -0700

    Initial test using socket.io Adding initial angular directory structure

---
+++ TODO.md
@@ -0,0 +1,7 @@
+## TODO
+* Modularize node backend
+* Create Angular UI & Controllers
+  * Create UI theme
+* Use gulp to generate index.html
+* Wrap javascript-bcrypt in angular service
+* pre-populate database
--- webapp/app.js
+++ webapp/app.js
@@ -1,20 +1,24 @@
 var express = require('express');
-var https = require('https');
-var fs = require('fs');
+var Server = require('socket.io');
+var http = require('http');
 var path = require('path');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var favicon = require('serve-favicon');
+
+var https = require('https');
+var fs = require('fs');
+
 var Datastore = require('nedb');
 
 var app = express();
+
 app.use(express.static(path.join(__dirname, 'public')));
 
 var protocol = process.argv[2] || "http"
 var port = protocol === "https" ? 443 : 80;
 
-// uncomment after placing your favicon in /public
 app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
@@ -26,19 +30,25 @@ app.get('*', function(req, res) {
 });
 
 //=========================================
-//TODO: npm install bcrypt --save
-//TODO: install Visual C++ VCBuild.exe (.NET Framework 2.0)
 var ds = new Datastore({filename: './users.db', autoload: true});
 //=========================================
 
+var appServer;
 if(protocol === "https") {
-  https.createServer({
+  appServer = https.createServer({
     key: fs.readFileSync('../key.pem'),
     cert: fs.readFileSync('../cert.pem')
-  },app).listen(port);
+  },app);
 } else {
-  app.listen(port);
+  appServer = http.createServer(app);
 }
+var io = new Server(appServer);
+appServer.listen(port);
+
+io.on('connection', function(socket) {
+  socket.join('console');
+  console.log("a user has connected");
+});
 console.log("Server running: "+protocol+"://localhost");
 
 module.exports = app;
